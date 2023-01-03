94a036406db7a0d53758243d9c30fa9926f3c187
POO <asada@hotmail.com>
2023-01-03 11:45:25 -0500

    menu por finalizar y a su vez ver si es rentable

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner lectura = new Scanner(System.in);
     public static void main(String[] args) {
+        int opcion;
         List listaMain = new ArrayList();
+        opcion = menu();
+        while (opcion != 0){
+            switch (opcion){
+
+                case 1:
+                    listaMain.add(new Vehiculo("Mercedes" , "E" , 55)) ;
+                    listaMain.add(new Vehiculo("Porsche" , "F10" , 250));
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }
+        System.out.println("Gracias por usar");
 
         listaMain.add(new Vehiculo("Mercedes" , "E" , 55)) ;
         listaMain.add(new Autobus("Mercedes" , "M20" , 100,20));
@@ -19,5 +42,22 @@ public class Main {
             v.imprimirAtributos();
             System.out.println("");
         }
+    }
+    private static int menu () {
+        int opcion = -1;
+        while (opcion < 0 || opcion > 3) {
+            System.out.println("Menu Patio Tuerca");
+            System.out.println("0.- Salir");
+            System.out.println("1.- Ingrese un nuevo Vehiculo");
+            System.out.println("2.- Ingrese un nuevo Autobus");
+            System.out.println("3.- Ingrese un nuevo vehiculo para Turismo");
+            System.out.println("ingrese una opcion ");
+            opcion = lectura.nextInt();
+
+            if (opcion < 0 || opcion > 3) {
+                System.out.println("Ingrese valores entre 1 y 3");
+            }
+        }
+        return opcion;
     }
 }
--- src/Turismo.java
+++ src/Turismo.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Turismo {
+public class Turismo extends Vehiculo{
+
+    private int numPuertas;
+
+    public Turismo(String marca, String modelo, double precioAlquiler, int numPuertas) {
+        super(marca, modelo, precioAlquiler);
+        setPuertas(numPuertas);
+    }
+
+    public void setPuertas(int numPuertas) {
+        this.numPuertas = numPuertas;
+    }
+
+    @Override
+    public void imprimirAtributos() {
+        super.imprimirAtributos();
+        System.out.print("\t" + "Puertas: " + numPuertas);
+    }
 }
